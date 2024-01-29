9b5374748bd06e40f2d2241768c869b5aed52f97
Syedfurkhan <marshalrap96@gmail.com>
2024-01-29 00:33:20 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about/about.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { ContactComponent } from './contact/contact/contact.component';
+import { LoginComponent } from './login/login/login.component';
+import { ProductsComponent } from './products/products/products.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    LayoutComponent,
+    ContactComponent,
+    LoginComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
