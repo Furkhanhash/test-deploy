c31e7f03613a906bb67d939389a86c4479801416
Syedfurkhan <marshalrap96@gmail.com>
2024-01-29 00:47:42 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login/login.component';
+import { LayoutComponent } from './layout/layout/layout.component';
+import { ProductsComponent } from './products/products/products.component';
+import { ContactComponent } from './contact/contact/contact.component';
+import { AboutComponent } from './about/about/about.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+ {
+  path:"",
+  redirectTo:"login",
+  pathMatch:'full',
+  
+
+ },
+ {
+  path:"login",
+  component:LoginComponent,
+  
+
+ },
+ {
+  path:'',
+  component:LayoutComponent,
+  children:[
+   {
+    path:"products",
+    component:ProductsComponent
+   },
+   {
+    path:"contact",
+    component:ContactComponent
+   },
+   {
+    path:"about",
+    component:AboutComponent
+   }
+  ]
+
+  }
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
